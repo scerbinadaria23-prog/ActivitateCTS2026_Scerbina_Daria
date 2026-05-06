539bc207e207c6e3588faed8e968cc1f410b137e
Daria Scerbina <dariascerbina23@stud.ase.ro>
2026-05-06 22:40:30 +0300

    Ninth commit - Seminar12 CTS

---
+++ Seminar12/ChainOfResponsability/src/ro/ase/cts/clase/CalatorieMetrou.java
@@ -0,0 +1,9 @@
+package ro.ase.cts.clase;
+
+public class CalatorieMetrou extends CalatorieHandler {
+
+    @Override
+    public String recomandaCalatorie(double distanta) {
+        return "Metrou";
+    }
+}
